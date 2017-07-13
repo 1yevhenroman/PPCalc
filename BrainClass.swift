875d2e27d283274d3851d4458f9db00ee9442006
yevhenRoman <yevhen_54@hotmail.com>
2017-07-13 15:42:56 +0300

    Fixed Decimal numbers
    Need to fix only double tap

--- BrainClass.swift
+++ BrainClass.swift
@@ -25,106 +25,112 @@ class Perfected: Model {
             .mns : TypeOperation.binaryOperation({$0 - $1}),
             .equal : TypeOperation.equals
     ]
-
+    
     
     var input: String = ""
     private let regex: String = "[-+]?\\d+.?\\d+"
-//    private var counterForNumber: Int = 0
-//    private var x: Double = 0
-//    private var y: Double = 0
-//    
-//    private var xSet: Bool = false
-//    private var ySet: Bool = false
+    //    private var counterForNumber: Int = 0
+    //    private var x: Double = 0
+    //    private var y: Double = 0
+    //
+    //    private var xSet: Bool = false
+    //    private var ySet: Bool = false
     var output = OutputAdapter.shared
     private var accumulator: Double?
-
-    //pretty good working
-//    private func number() {
-//        var num = input.components(separatedBy: ["+", "*", "/","-"])
-//        
-//        if !(xSet && ySet) {
-//            x = Double(num[0])!
-//            y = Double(num[1])!
-//            return
-//        }
-//        if !xSet {
-//            x = Double(num[0])!
-//        }
-//        else {
-//            y = Double(num[0])!
-//        }
-//    }
-        enum TypeOperation {
-            case constant(Double)
-            case unaryOperation((Double)->Double)
-            case binaryOperation((Double,Double)->Double)
-            case equals
-        }
-        
-        struct PendingBinaryOperation {
-            let function: (Double,Double)->Double
-            let firstOperand: Double
-            func perform(with secondOperand: Double)->Double{
-                return function(firstOperand, secondOperand)
-            }
-        }
-        var pendingBinaryOperation: PendingBinaryOperation?
     
-        func performOperation(_ symbol: Operation) {
-            if let operation = operations[symbol] {
-                
-                switch operation {
-                case .constant(let value): accumulator = value
-                case .unaryOperation(let function):
-                    if accumulator != nil {
-                        accumulator = function(accumulator!)}
-                case .binaryOperation(let function):
-                    
-                    if accumulator != nil {
-                        pendingBinaryOperation=PendingBinaryOperation(function: function, firstOperand: accumulator!)
-                        accumulator=nil
-                    }
-                case .equals():
-                    performPendingBinaryOperation()            }
-            }
-            //output.presentResult(result: result)
+    //pretty good working
+    //    private func number() {
+    //        var num = input.components(separatedBy: ["+", "*", "/","-"])
+    //
+    //        if !(xSet && ySet) {
+    //            x = Double(num[0])!
+    //            y = Double(num[1])!
+    //            return
+    //        }
+    //        if !xSet {
+    //            x = Double(num[0])!
+    //        }
+    //        else {
+    //            y = Double(num[0])!
+    //        }
+    //    }
+    enum TypeOperation {
+        case constant(Double)
+        case unaryOperation((Double)->Double)
+        case binaryOperation((Double,Double)->Double)
+        case equals
+    }
+    
+    struct PendingBinaryOperation {
+        let function: (Double,Double)->Double
+        let firstOperand: Double
+        func perform(with secondOperand: Double)->Double{
+            return function(firstOperand, secondOperand)
         }
-        
-         func performPendingBinaryOperation(){
-            if pendingBinaryOperation != nil && accumulator != nil {
-                accumulator = pendingBinaryOperation!.perform(with: accumulator!)}
-                pendingBinaryOperation = nil
+    }
+    var pendingBinaryOperation: PendingBinaryOperation?
+    
+    func performOperation(_ symbol: Operation) {
+        if let operation = operations[symbol] {
             
+            switch operation {
+            case .constant(let value): accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)}
+            case .binaryOperation(let function):
+                
+                if accumulator != nil {
+                    pendingBinaryOperation=PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    accumulator=nil
+                }
+            case .equals():
+                performPendingBinaryOperation()            }
+        }
+        //output.presentResult(result: result)
+    }
+    
+    func performPendingBinaryOperation(){
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)}
+        pendingBinaryOperation = nil
+        
+    }
+    
+    func setOperand( _ operand: String) {
+        if let number = Double(operand)  {
+            accumulator = number
         }
         
-         func setOperand( _ operand: String) {
-           
-            let subStringArray = operand.stringsMatchingRegularExpression(expression: "[-+]?\\d+.?\\d+")
-            if let number = subStringArray?[0] {
-                accumulator = Double(number)
-            }
-           
+        
+        
     }
     func printOnScreen () {
-     output.presentResult(result: input)
+        output.presentResult(result: input)
     }
     var result: String {
-        get {//Поставити усмови для комфортного виводу
+        get {
             if let res = accumulator {
+                var modifiedResult = String(res)
+                if modifiedResult.hasSuffix(".0") {
+                    let range = modifiedResult.index(modifiedResult.endIndex, offsetBy: -2)..<modifiedResult.endIndex
+                    modifiedResult.removeSubrange(range)
+                    return modifiedResult
+                }
                 return String(res)
             }
             else {
-              return "Error"
+                return " "
             }
         }
     }
     
-        func enterEquation(equation: String) {
-            
-        }
+    func enterEquation(equation: String) {
         
-   
+    }
     
-
+    
+    
+    
     
 }
